8ddac9afd14a2a7066860caac28445fc124a9579
ddddddO <lbfdeatq@gmail.com>
2021-08-21 11:33:33 +0900

    Modify as を連番で生成

--- example_gdag_test.go
+++ example_gdag_test.go
@@ -5,22 +5,22 @@ import (
 )
 
 func Example() {
-	goal := g.NewGoal("ゴール(目的)", "goal")
+	goal := g.NewGoal("ゴール(目的)")
 
-	design := g.NewTask("設計", "design")
-	review_design := g.NewTask("レビュー対応", "review_design")
+	design := g.NewTask("設計")
+	review_design := g.NewTask("レビュー対応")
 
-	develop_feature_1 := g.NewTask("feature1開発", "develop_feature_1")
+	develop_feature_1 := g.NewTask("feature1開発")
 	develop_feature_1.AddNote("xxが担当")
-	review_develop_feature_1 := g.NewTask("レビュー対応", "review_develop_feature_1")
+	review_develop_feature_1 := g.NewTask("レビュー対応")
 
-	develop_feature_2 := g.NewTask("feature2開発", "develop_feature_2")
+	develop_feature_2 := g.NewTask("feature2開発")
 	develop_feature_2.AddNote("yyが担当")
-	review_develop_feature_2 := g.NewTask("レビュー対応", "review_develop_feature_2")
+	review_develop_feature_2 := g.NewTask("レビュー対応")
 
-	test := g.NewTask("結合テスト", "test")
-	release := g.NewTask("リリース", "release")
-	finish := g.NewTask("finish", "finish")
+	test := g.NewTask("結合テスト")
+	release := g.NewTask("リリース")
+	finish := g.NewTask("finish")
 
 	goal.Connect(design).Connect(review_design).Connect(develop_feature_1).Connect(review_develop_feature_1).Connect(test)
 	review_design.Connect(develop_feature_2).Connect(review_develop_feature_2).Connect(test)
@@ -31,36 +31,37 @@ func Example() {
 	}
 	// Output:
 	// @startuml
-	// rectangle "ゴール(目的)" as goal
-	// usecase "設計" as design
-	// usecase "レビュー対応" as review_design
-	// usecase "feature1開発" as develop_feature_1
+	// rectangle "ゴール(目的)" as 1
+	// usecase "設計" as 2
+	// usecase "レビュー対応" as 3
+	// usecase "feature1開発" as 4
 	// note left
 	// xxが担当
 	// end note
-	// usecase "レビュー対応" as review_develop_feature_1
-	// usecase "結合テスト" as test
-	// usecase "リリース" as release
-	// usecase "finish" as finish
-	// usecase "feature2開発" as develop_feature_2
+	// usecase "レビュー対応" as 5
+	// usecase "結合テスト" as 8
+	// usecase "リリース" as 9
+	// usecase "finish" as 10
+	// usecase "feature2開発" as 6
 	// note left
 	// yyが担当
 	// end note
-	// usecase "レビュー対応" as review_develop_feature_2
-	// usecase "結合テスト" as test
-	// usecase "リリース" as release
-	// usecase "finish" as finish
+	// usecase "レビュー対応" as 7
+	// usecase "結合テスト" as 8
+	// usecase "リリース" as 9
+	// usecase "finish" as 10
 
-	// goal --> design
-	// design --> review_design
-	// review_design --> develop_feature_1
-	// develop_feature_1 --> review_develop_feature_1
-	// review_develop_feature_1 --> test
-	// test --> release
-	// release --> finish
-	// review_design --> develop_feature_2
-	// develop_feature_2 --> review_develop_feature_2
-	// review_develop_feature_2 --> test
+	// 1 --> 2
+	// 2 --> 3
+	// 3 --> 4
+	// 4 --> 5
+	// 5 --> 8
+	// 8 --> 9
+	// 9 --> 10
+	// 3 --> 6
+	// 6 --> 7
+	// 7 --> 8
 
 	// @enduml
+
 }
--- gdag.go
+++ gdag.go
@@ -12,26 +12,33 @@ const (
 type Node struct {
 	nodeType nodeType
 	text     string
-	as       string // plantumlでの識別子。自動で生成したいけど、例えばa, b, ...とかにしちゃうと、他の人がplantumlを編集するとき辛くなる。あと、識別子なので重複する場合はエラーとする。
+
+	// plantumlでの識別子。自動で生成したいけど、例えばa, b, ...とかにしちゃうと、他の人がplantumlを編集するとき辛くなる。あと、識別子なので重複する場合はエラーとする。
+	// の予定だったが、自動で生成する。連番。たぶん他の人がumlいじることはないとも思う。
+	as       int
 	note     string
 	parents  []*Node
 	children []*Node
 }
 
-func newNode(nodeType nodeType, text, as string) *Node {
+var nodeIdx int
+
+func newNode(nodeType nodeType, text string) *Node {
+	nodeIdx++
+
 	return &Node{
 		nodeType: nodeType,
 		text:     text,
-		as:       as,
+		as:       nodeIdx,
 	}
 }
 
-func NewGoal(text, as string) *Node {
-	return newNode(rectangle, text, as)
+func NewGoal(text string) *Node {
+	return newNode(rectangle, text)
 }
 
-func NewTask(text, as string) *Node {
-	return newNode(usecase, text, as)
+func NewTask(text string) *Node {
+	return newNode(usecase, text)
 }
 
 func (upstream *Node) Connect(current *Node) *Node {
@@ -70,10 +77,10 @@ func generateComponents(goal *Node) string {
 var dstComponents string // 要リファクタ
 func generateComponent(n *Node) {
 	if n.nodeType == rectangle {
-		dstComponents += fmt.Sprintf("rectangle \"%s\" as %s\n", n.text, n.as)
+		dstComponents += fmt.Sprintf("rectangle \"%s\" as %d\n", n.text, n.as)
 	}
 	if n.nodeType == usecase {
-		dstComponents += fmt.Sprintf("usecase \"%s\" as %s\n", n.text, n.as)
+		dstComponents += fmt.Sprintf("usecase \"%s\" as %d\n", n.text, n.as)
 	}
 	if len(n.note) != 0 {
 		dstComponents += fmt.Sprintf("note left\n%s\nend note\n", n.note)
@@ -93,15 +100,15 @@ func generateRelations(goal *Node) string {
 var uniq = make(map[string]struct{})
 
 func generateRelation(n *Node) {
-	r := fmt.Sprintf("%s --> ", n.as)
+	r := fmt.Sprintf("%d --> ", n.as)
 	for _, child := range n.children {
-		key := n.as + child.as
+		key := fmt.Sprintf("%d%d", n.as, child.as)
 		if _, ok := uniq[key]; ok {
 			continue
 		}
 		uniq[key] = struct{}{}
 
-		dstRelations += r + child.as + "\n"
+		dstRelations += fmt.Sprintf("%s%d\n", r, child.as)
 		generateRelation(child)
 	}
 }
